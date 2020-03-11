74466e24f7dd4ac0ce199d9d856c215cc256cb43
g5sh <eagomez2387@gmail.com>
2020-03-11 13:05:30 -0500

    [FIX]TAKS 1: Add free and comments to point 7

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,9 +14,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	binary_tree_t *new_node = NULL;
 
-	if (parent == NULL)
-		return (NULL);
-
 	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
@@ -24,9 +21,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 
-	new_node->parent = parent;
-
 	new_node->right = NULL;
+
+	new_node->left = NULL;
+
+	if (parent == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->parent = parent;
 
 	if (parent->left != NULL)
 	{
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - print binary tree inorder
+ * @tree: input entire tree
+ * @func: input print function
+ * Return: void function
+ */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL && func != NULL)
